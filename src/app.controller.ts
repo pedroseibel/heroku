c6502bca3ef5787de5a3ab53dbe48084196be944
Pedro Seibel <64672223+pedroseibel@users.noreply.github.com>
2022-01-05 21:44:39 -0300

    Routes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,17 +5,17 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('hello')
   getHello(): string {
     return this.appService.getHello();
   }
 
-  @Get()
+  @Get('list')
   getList(): string[] {
     return this.appService.getList();
   }
 
-  @Get()
+  @Get('objlist')
   getObjList(): object[] {
     return this.appService.getList2();
   }
